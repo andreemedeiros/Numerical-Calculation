f9f27ae1c23b28bf8dc699677cb7fef0338c41cb
andreemedeiros <andre.escariao1@gmail.com>
2024-04-24 17:34:05 -0300

    at

--- Listas/Lista_06_Interpolacao/exercicio_13_1.py
+++ Listas/Lista_06_Interpolacao/exercicio_13_1.py
@@ -1,38 +1,39 @@
-# Solução exercício 13.1 
+# Solução exercício 13.1
 
 import numpy as np
 
-# Definindo os valores de x e y
+# Dados fornecidos
 x = np.array([0, 1, 2.5, 3, 4.5, 5, 6])
 y = np.array([2, 5.4375, 7.3516, 7.5625, 8.4453, 9.1875, 12])
 
-# Criando a tabela de diferenças divididas finitas
-n = len(x)
-diff_div = np.zeros((n, n))
+# Calculando as diferenças divididas finitas
+def divided_differences(x, y):
+    n = len(y)
+    coef = np.zeros(n)
+    coef[0] = y[0]
+    
+    for j in range(1, n):
+        for i in range(n-1, j-1, -1):
+            y[i] = (y[i] - y[i-1]) / (x[i] - x[i-j])
+        coef[j] = y[j]
+    
+    return coef
 
-# Diferenças divididas de primeira ordem
-diff_div[:, 0] = y
+# Construindo o polinômio interpolador de Newton
+def newton_interpolation(x, coef, xi):
+    n = len(x) - 1
+    p = coef[n]
+    
+    for k in range(n-1, -1, -1):
+        p = coef[k] + (xi - x[k]) * p
+    
+    return p
 
-# Diferenças divididas de ordens superiores
-for i in range(1, n):
-    for j in range(n-i):
-        diff_div[j, i] = (diff_div[j+1, i-1] - diff_div[j, i-1]) / (x[j+i] - x[j])
+# Calculando as diferenças divididas
+coef = divided_differences(x, y)
 
-# Função para calcular o valor de y em um ponto x usando o polinômio de Newton
-def newton_polynomial(x, x_data, y_data, diff_div):
-    n = len(x_data)
-    result = y_data[0]
+# Avaliando o polinômio interpolador em x = 3.5
+xi = 3.5
+y_interpolated = newton_interpolation(x, coef, xi)
 
-    for i in range(1, n):
-        term = diff_div[0, i]
-        for j in range(1, i+1):
-            term *= (x - x_data[j])
-        result += term
-
-    return result
-
-# Calculando o valor de y em x = 3.5
-x_target = 3.5
-y_interpolated = newton_polynomial(x_target, x, y, diff_div)
-
-print("Valor interpolado de y em x = 3.5:", y_interpolated)
+print(f"O valor interpolado de y para x = {xi} é {y_interpolated:.4f}")
